4e49774dc5c5baf39a1a33beb32a686e4df5c5c7
moremind <hefengen@hotmail.com>
2023-04-17 23:51:53 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,33 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [
-      vue(),
-      vueJsx(),
-      AutoImport({
-          resolvers: [ElementPlusResolver()],
-      }),
-      Components({
-          resolvers: [ElementPlusResolver()],
-      }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: './',
+    plugins: [
+        vue(),
+        vueJsx(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
+    // resolve: {
+    //     alias: {
+    //         '@': fileURLToPath(new URL('./src', import.meta.url))
+    //     }
+    // },
+    optimizeDeps: {
+        include: ['schart.js']
     }
-  }
 })
